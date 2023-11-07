83585149ce1997b8dac80fc46faaf95cf05d9861
Ye Myo Aung <yemyo994@gmail.com>
2023-11-07 19:18:11 +0630

    Day 3 Lessions

---
+++ lib/dart_programming.dart
@@ -0,0 +1,46 @@
+/// Variable
+///
+/// x = 1
+/// y = 2
+/// z = x + y
+/// x = နေကောင်းလား "",''
+
+///DATATYPE Name (){}
+void main() {
+  /// Basic
+  /// Keyword (final,const,late)
+  /// DATATYPE (Null Safety)
+  /// NAME
+  /// VALUE
+  ///* kdnv
+  /// int,String,double,bool,List,Map
+  ///
+  /// const
+  /// final
+  /// late
+  ///
+  //true,false,null
+
+  // null  => NULL
+  // blank =>
+  // space => " "
+
+  ///
+
+  String? name; //null
+  //!
+  print(name!);
+
+  // var,dynamic
+
+  // var a = {};
+  // a = {[]: []};
+
+  // var a = 1;
+
+  // String staticName = '1';
+  // staticName.toUpperCase();
+
+  // dynamic dynamicName = 1;
+  // dynamicName.toUpperCase();
+}
