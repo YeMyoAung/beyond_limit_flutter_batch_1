23e2898087e1b4a356179cbd15f7ec6f417446a8
Ye Myo Aung <yemyo994@gmail.com>
2023-11-09 20:37:31 +0630

    Day 7 List Useful Function

--- lib/functional_programming/list/list.dart
+++ lib/functional_programming/list/list.dart
@@ -42,9 +42,6 @@ void main() {
 
   ///reversed
 
-  // names.where(element);
-
-
   // names.addAll(["1", "2", '3']);
   // print(names);
   // names.clear();
@@ -54,6 +51,61 @@ void main() {
   // print(names);
   // names.remove('mg mg');
   // print(names);
+  final List<String> dataset = ['mg mg', 'aung aung', 'su su', 'aye aye'];
+  // String result = dataset.firstWhere(
+  //   (String value) {
+  //     return value == 'mg mg';
+  //   },
+  //   orElse: () => 'Not Found',
+  // );
+  // print(result);
+  final String firstResult = dataset.firstWhere((value) => value.contains('g'));
+  print(firstResult);
+  final String lastResult = dataset.lastWhere((value) {
+    return value.contains('z');
+  }, orElse: () => 'Not Found');
+  print(lastResult);
+  final whereResult = dataset.where((value) {
+    return value.contains('a');
+  });
+  print(whereResult.toList());
+
+  final int indexResult = dataset.indexWhere((value) {
+    /// index number = 0
+    return value.startsWith('a');
+  });
+
+  print(indexResult);
+
+  final int lastIndexResult = dataset.lastIndexWhere((value) {
+    /// index number = 0
+    return value.startsWith('a');
+  });
+  print(lastIndexResult);
+
+  // dataset.removeWhere((element) {
+  //   return element == 'mg mg';
+  // });
+
+  // print(dataset);
+
+  // dataset.retainWhere((element) {
+  //   return element.startsWith('a');
+  // });
+  print(dataset);
+  dataset.sort((child, current) {
+    return child.compareTo(current);
+  });
+  print(dataset);
+
+  ///LOOP => limit
+  ///map,forEach == for in
+  final mapResult = dataset.map<String>((a) {
+    return "HELLO " + a;
+  });
+  //['value',]//index
+  ///{
+  ///index:value,
+  ///}
+  print(mapResult);
 }
-
-
