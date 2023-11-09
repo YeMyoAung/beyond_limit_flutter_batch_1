e58810ff1c3293ad40d1a9cdaf8454f1e7f55b79
Ye Myo Aung <yemyo994@gmail.com>
2023-11-09 20:36:46 +0630

    Day 7 Revesion

---
+++ lib/basic/rev.dart
@@ -0,0 +1,26 @@
+void main() {
+  ///Variable
+  ///DNV
+  ///DataType Name Value
+  String name;
+  name = "jfkajdf";
+  question('fjkladf');
+
+  ///String,int,double,List,Map,bool,Function,null
+  ///
+  ///String,int,double,bool
+  ///List<e>,Map<k,v>
+  ///Datatype Function(argvs)
+
+  test((String name, [String? value]) {
+    ///
+  });
+}
+
+void test(
+  void Function(String name, [String value]) task,
+) {}
+
+void question(String value) {}
+
+dynamic answer() {}
